86ab0c539ee7a65aa749962ef27dccb011785974
connie-reinholdsson <connie.reinholdsson@gmail.com>
2020-10-11 11:57:21 +0100

    Commented out code not needed.

--- src/Application.kt
+++ src/Application.kt
@@ -34,43 +34,45 @@ fun Application.module(testing: Boolean = false) {
     }
 
     routing {
-        get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-        }
-
-        get<MyLocation> {
-            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
-        }
-        // Register nested routes
-        get<Type.Edit> {
-            call.respondText("Inside $it")
-        }
-        get<Type.List> {
-            call.respondText("Inside $it")
-        }
-
-        get("/session/increment") {
-            val session = call.sessions.get<MySession>() ?: MySession()
-            call.sessions.set(session.copy(count = session.count + 1))
-            call.respondText("Counter is ${session.count}. Refresh to increment.")
-        }
-
-        get("/json/gson") {
-            call.respond(mapOf("hello" to "world"))
-        }
+        // Don't need this
+//        get("/") {
+//            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+//        }
+//
+//        get<MyLocation> {
+//            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
+//        }
+//        // Register nested routes
+//        get<Type.Edit> {
+//            call.respondText("Inside $it")
+//        }
+//        get<Type.List> {
+//            call.respondText("Inside $it")
+//        }
+//
+//        get("/session/increment") {
+//            val session = call.sessions.get<MySession>() ?: MySession()
+//            call.sessions.set(session.copy(count = session.count + 1))
+//            call.respondText("Counter is ${session.count}. Refresh to increment.")
+//        }
+//
+//        get("/json/gson") {
+//            call.respond(mapOf("hello" to "world"))
+//        }
     }
 }
 
-@Location("/location/{name}")
-class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
-
-@Location("/type/{name}") data class Type(val name: String) {
-    @Location("/edit")
-    data class Edit(val type: Type)
-
-    @Location("/list/{page}")
-    data class List(val type: Type, val page: Int)
-}
+// Don't need this
+//@Location("/location/{name}")
+//class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
+//
+//@Location("/type/{name}") data class Type(val name: String) {
+//    @Location("/edit")
+//    data class Edit(val type: Type)
+//
+//    @Location("/list/{page}")
+//    data class List(val type: Type, val page: Int)
+//}
 
 data class MySession(val count: Int = 0)
 
